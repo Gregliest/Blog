5b55c6a2f8d0a2c87f3acce9fca8b47f4fa23d0c
Greg <gregliest@gmail.com>
2022-11-21 22:20:16 -0800

    Maybe fix background attachment

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { getAllDisplayPosts } from '@/lib/utils/contentProvider'
 import ArticleGallery from '@/components/ArticleGallery'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import Image from '@/components/Image'
 import AnimatedTextBox from '@/components/AnimatedTextBox'
 
 export const POSTS_PER_PAGE = 5
@@ -30,21 +31,23 @@ export default function Blog({ allPosts }: InferGetStaticPropsType<typeof getSta
   const blogAbout = ['Software', 'Whitewater', 'Photography', 'Rescue', 'and More!']
   return (
     <div>
-      <div className="h-screen w-full bg-bowser bg-cover bg-fixed bg-center">
-        <Header />
-
-        <div className="h-1/4" />
-        <AnimatedTextBox title="A Blog About:" array={blogAbout} />
-        <div className="h-1/4" />
-
-        <PageSEO
-          title={`Projects - ${siteMetadata.author}`}
-          description={siteMetadata.description}
+      <div className="fixed -z-10 h-full w-full">
+        <Image
+          alt="Background"
+          src="/static/images/photography/capitol-reef-tree.jpg"
+          fill
+          objectFit="cover"
         />
-
-        <ArticleGallery articles={allPosts} />
-        <Footer />
       </div>
+      <Header />
+
+      <div className="h-1/4" />
+      <AnimatedTextBox title="A Blog About:" array={blogAbout} />
+      <div className="h-1/4" />
+
+      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <ArticleGallery articles={allPosts} />
+      <Footer />
     </div>
   )
 }
