feecb8659a395e62f5560f5ac8b6b9d82c4af540
Greg <gregliest@gmail.com>
2022-11-23 21:34:01 -0800

    Add code background photo

--- pages/code.tsx
+++ pages/code.tsx
@@ -6,6 +6,7 @@ import ArticleGallery from '@/components/ArticleGallery'
 import { MinimalHeader } from '@/components/Header'
 import Footer from '@/components/Footer'
 import { filterByTag } from '@/lib/utils/posts'
+import Image from '@/components/Image'
 
 export const getStaticProps = async () => {
   const allPosts = await getAllDisplayPosts()
@@ -19,12 +20,29 @@ export const getStaticProps = async () => {
 }
 export default function Code({ codePosts }) {
   return (
-    <div className="h-screen w-full bg-bowser bg-cover bg-fixed bg-center">
-      <MinimalHeader title="Code" />
-      <div className="mb-6 h-4 bg-gradient-to-b from-white to-transparent" />
-      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <ArticleGallery articles={codePosts} />
-      <Footer />
+    <div>
+      <div className="fixed -z-10 h-screen w-screen">
+        <Image
+          alt="Background"
+          src="/static/images/photography/capitol-reef-tree.jpg"
+          fill
+          objectFit="cover"
+          priority
+          sizes="4032px"
+          quality={100}
+        />
+      </div>
+
+      <div className="h-screen w-screen">
+        <MinimalHeader title="Code" />
+        <div className="mb-6 h-4 bg-gradient-to-b from-white to-transparent" />
+        <PageSEO
+          title={`Projects - ${siteMetadata.author}`}
+          description={siteMetadata.description}
+        />
+        <ArticleGallery articles={codePosts} />
+        <Footer />
+      </div>
     </div>
   )
 }
