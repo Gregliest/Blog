0fe3d1cfc05e603c1c079ee7fc6619a3c6418117
Greg <gregliest@gmail.com>
2022-12-03 20:38:15 -0800

    Fix article card layout

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -28,27 +28,30 @@ export default function ArticleCard(props: ArticleCardProps) {
   return (
     <div className="group relative w-96 shrink-0 drop-shadow-lg">
       <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-slate-500 to-zinc-700 opacity-25 blur transition duration-700 group-hover:opacity-100 group-hover:duration-200"></div>
-      <div className="relative flex h-full w-full space-x-6 rounded-lg bg-white leading-none ring-1 ring-gray-900/5">
+      <div className="relative h-full w-full space-x-6 rounded-lg bg-white leading-none ring-1 ring-gray-900/5">
         <Link href={link} aria-label={`Link to ${link}`}>
-          {props.post.thumbnail && (
-            <Image
-              alt={props.post.title}
-              src={props.post.thumbnail}
-              width={450}
-              height={306}
-              className="rounded-t-lg"
-            />
-          )}
-          <div className="divide-y divide-gray-200 py-6 px-8">
-            <div>
-              <p className="w-full text-center text-2xl">{props.post.title}</p>
-              <p className="mb-2 w-full text-right text-sm italic text-gray-500">
-                {formatDate(props.post.date)}
-              </p>
+          <div className="flex h-full w-full flex-col">
+            {props.post.thumbnail && (
+              <Image
+                alt={props.post.title}
+                src={props.post.thumbnail}
+                width={450}
+                height={306}
+                className="rounded-t-lg"
+              />
+            )}
+            <div className="divide-y divide-gray-200 py-6 px-8">
+              <div>
+                <p className="w-full text-center text-2xl">{props.post.title}</p>
+                <p className="mt-2 mb-1 w-full text-right text-sm italic text-gray-500">
+                  {formatDate(props.post.date)}
+                </p>
+              </div>
+              <p className="pt-4 text-sm">{props.post.summary}</p>
             </div>
-            <p className="pt-4 text-sm">{props.post.summary}</p>
+            <div className="flex-grow" />
+            {getReadFull(props.post)}
           </div>
-          {getReadFull(props.post)}
         </Link>
       </div>
     </div>
