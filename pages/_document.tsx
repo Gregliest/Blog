354f8308fdacf804e02c511006961708637bb08e
Gregory Lee <git@gregliest.com>
2022-12-08 17:49:03 -0800

    Add next logo iteration.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,8 +20,8 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#2b5797" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="msapplication-TileColor" content="#2d89ef"/>
+          <meta name="theme-color" content="#ffffff"/>
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
