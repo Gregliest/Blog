2ba63f13d6edfc82e83cc6f06227b1a108e93f6f
Greg <gregliest@gmail.com>
2023-03-11 19:49:59 -0800

    Delete mixpanel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,21 +11,15 @@ import Head from 'next/head'
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import { ClientReload } from '@/components/ClientReload'
-import mixpanel from 'mixpanel-browser'
 import { useEffect } from 'react'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
-const mixpanelKey = process.env.MIXPANEL_KEY
-
-// mixpanel.init('df1dbb298fa300d458ecef32bb7b161b', { debug: isDevelopment })
-mixpanel.init('df1dbb298fa300d458ecef32bb7b161b')
 
 export default function App({ Component, pageProps, router }: AppProps) {
   useEffect(() => {
     const handleRouteChange = (url) => {
       console.log('Sending event ' + url)
-      mixpanel.track(url, document.title)
     }
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
